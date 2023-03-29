fb96562870bb3348c6fae5e0eb48c5bc6ad8b774
aidangoldie7 <40402670@live.napier.ac.uk>
2023-03-29 13:03:57 +0100

    7. Create Sign Up Page

---
+++ app/src/main/java/com/example/chatapplication/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapplication.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
