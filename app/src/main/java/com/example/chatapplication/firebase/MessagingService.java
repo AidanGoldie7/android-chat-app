5989f046005fa7e29d1ff5b96b692cd0d680327d
aidangoldie7 <40402670@live.napier.ac.uk>
2023-04-19 16:00:21 +0100

    29. Updates to MessagingService.java

--- app/src/main/java/com/example/chatapplication/firebase/MessagingService.java
+++ app/src/main/java/com/example/chatapplication/firebase/MessagingService.java
@@ -1,4 +1,26 @@
 package com.example.chatapplication.firebase;
 
-public class MessagingService {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+
+    @Override
+    public void onNewToken(@NonNull String token){
+        super.onNewToken(token);
+        Log.d("FCM", "Token: " + token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+        Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
+    }
+
+
 }
